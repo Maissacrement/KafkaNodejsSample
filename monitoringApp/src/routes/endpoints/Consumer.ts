c42d244146dd91ee5d0f2a13a9c08e1b40208aec
mario <emmariodelar@gmail.com>
2019-07-26 12:30:45 +0200

    listen consumer

--- monitoringApp/src/routes/endpoints/Consumer.ts
+++ monitoringApp/src/routes/endpoints/Consumer.ts
@@ -2,27 +2,16 @@ import kafka = require("kafka-node");
 const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient({ kafkaHost: "127.0.0.1:9092" });
 
-const receiveMsgFrom1Topic = (err: any, res: any) => {
-  if(err){
-    throw new Error(err);
+const consumer = new Consumer(
+  client,
+  [{ topic: "hello-world", partition: 0 }],
+  {
+    autoCommit: false
   }
+);
 
-  const consumer = new Consumer(
-    client,
-    [{ topic: "hello-world", partition: 0 }],
-    {
-      autoCommit: false
-    }
-  );
+consumer.on("message", async response => {
+  process.stdout.write("\nTopic receive message by Consumer:\n");
+  process.stdout.write(JSON.stringify(response, null, " ") + "\n");
 
-  consumer.on("message", async response => {
-    process.stdout.write("\nTopic receive message by Consumer:\n");
-    process.stdout.write(JSON.stringify(response, null, " ") + "\n");
-    res.json({
-      message: response,
-      status: 200
-    });
-  });
-};
-
-export { receiveMsgFrom1Topic };
+});
--- monitoringApp/src/routes/index.ts
+++ monitoringApp/src/routes/index.ts
@@ -3,13 +3,12 @@ import express = require("express");
 const router: any = express.Router();
 
 // Import Pages
+import "./endpoints/Consumer";
 import BadRequest from "./endpoints/badRequest";
-import { receiveMsgFrom1Topic } from "./endpoints/Consumer";
 import { createANewTopic, sendTopic } from "./endpoints/Producerv2";
 
 router.get("/sendTopicv2", sendTopic);
 router.get("/createTopicv2", createANewTopic);
-router.get("/listen", receiveMsgFrom1Topic);
 router.get("/*", BadRequest);
 
 export default router;
